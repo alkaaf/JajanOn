c9423318539280cd517fefc2e8b269fe4e22efec
alkaaf <alfa.alkaaf@gmail.com>
2016-07-07 20:22:54 +0700

    foto bunder

--- app/src/main/java/com/dalbo/jajanon/Adapt/listview/HomeListRowView.java
+++ app/src/main/java/com/dalbo/jajanon/Adapt/listview/HomeListRowView.java
@@ -1,12 +1,16 @@
 package com.dalbo.jajanon.Adapt.listview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.dalbo.jajanon.CustomClass.Rounder;
 import com.dalbo.jajanon.Entity.HomeList;
 import com.dalbo.jajanon.R;
 
@@ -32,6 +36,8 @@ public class HomeListRowView extends ArrayAdapter<HomeList> {
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = li.inflate(R.layout.n_home_listrowview,parent,false);
         TextView usaha, alamat, jarak, status;
+        ImageView avatar;
+        avatar = (ImageView)v.findViewById(R.id.img_cover);
         usaha = (TextView)v.findViewById(R.id.judul);
         alamat = (TextView)v.findViewById(R.id.alamat);
         jarak = (TextView)v.findViewById(R.id.jarak);
@@ -46,6 +52,8 @@ public class HomeListRowView extends ArrayAdapter<HomeList> {
         else if(data.get(position).getStatus().equalsIgnoreCase("tutup")){
             status.setTextColor(c.getResources().getColor(R.color.tutup));
         }
+        Bitmap bm = BitmapFactory.decodeResource(getContext().getResources(),R.drawable.dummy_avatar);
+        avatar.setImageDrawable(new Rounder(bm));
         return v;
     }
 }
--- app/src/main/java/com/dalbo/jajanon/ProfileActivity.java
+++ app/src/main/java/com/dalbo/jajanon/ProfileActivity.java
@@ -1,23 +1,30 @@
 package com.dalbo.jajanon;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.dalbo.jajanon.Adapt.pager.ProfilePager;
+import com.dalbo.jajanon.CustomClass.Rounder;
 
 /**
  * Created by alkaaf on 7/7/2016.
  */
 public class ProfileActivity extends AppCompatActivity {
-
+    ImageView avatar;
     ViewPager vp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.n_profile);
+        avatar = (ImageView)findViewById(R.id.img_avatar);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.dummy_avatar);
+        avatar.setImageDrawable(new Rounder(bm));
         vp = (ViewPager)findViewById(R.id.profile_content);
         vp.setAdapter(new ProfilePager(getSupportFragmentManager(),vp));
     }
