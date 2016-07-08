d08b6adf9faa05fb46575bcb22ec2d3e71762c73
alkaaf <alfa.alkaaf@gmail.com>
2016-07-08 07:30:26 +0700

    working map

--- app/src/main/java/com/dalbo/jajanon/Frag/f_lapak/lokasi.java
+++ app/src/main/java/com/dalbo/jajanon/Frag/f_lapak/lokasi.java
@@ -1,28 +1,47 @@
 package com.dalbo.jajanon.Frag.f_lapak;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dalbo.jajanon.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by alkaaf on 7/7/2016.
  */
-public class lokasi extends Fragment {
+public class lokasi extends Fragment implements OnMapReadyCallback {
+    MapView mapv;
+    GoogleMap gmap;
+    LatLng ll;
     public lokasi() {
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.n_lapak_lokasi,container,false);
-
-//        SupportMapFragment mapFragment = getChildFragmentManager().findFragmentById();
+        View v = inflater.inflate(R.layout.n_lapak_lokasi, container, false);
+        mapv = (MapView) v.findViewById(R.id.mapv);
+        mapv.onCreate(savedInstanceState);
+        mapv.onResume();
+        mapv.getMapAsync(this);
         return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.gmap = googleMap;
+
+        gmap.setMyLocationEnabled(true);
     }
 }
